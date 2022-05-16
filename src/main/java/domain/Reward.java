18b3466f4e41621791886b8f1316781de7c90a4c
seok2 <seogi89@gmail.com>
2022-05-16 23:20:11 +0900

    <feat> Reword가 보너스번호를 입력받을 수 있도록 한다.

--- src/main/java/domain/Reward.java
+++ src/main/java/domain/Reward.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public enum Reward {
 
     FIRST(6, new Money(200_000_000L)),
-    SECOND(5, new Money(15_000_00)),
-    THIRD(4, new Money(50_000)),
-    FOURTH(3, new Money(5_000)),
+    SECOND(5, new Money(30_000_00)),
+    THIRD(5, new Money(15_000_00)),
+    FOURTH(4, new Money(50_000)),
+    FIFTH(3, new Money(5_000)),
     MISS(0, new Money(0));
 
     private final int numberOfMatches;
@@ -18,9 +19,10 @@ public enum Reward {
         this.prize = prize;
     }
 
-    public static Reward of(int matches) {
+    public static Reward of(int matches, boolean bonus) {
         return Arrays.stream(values())
                 .filter(reward -> reward.numberOfMatches == matches)
+                .filter(reward -> reward != SECOND || bonus)
                 .findAny()
                 .orElse(MISS);
     }
